1ca64960d8b0c3464434e0dbb8ef7a98e97712c8
xjw <857986029@qq.com>
2020-04-14 21:58:10 +0800

    首页header 头部引导导航

---
+++ components/public/header/geo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"></i>北京市
+    <nuxt-link class="changeCity" to="/changeCity">
+      切换城市
+    </nuxt-link>
+    [上海 天津 深圳]
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
