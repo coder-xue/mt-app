0b3dfdda824f4ab4ef93289d12c7befd9d75f2ab
xjw <857986029@qq.com>
2020-04-16 17:30:48 +0800

    首页footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,20 @@
     <el-main>
       <nuxt />
     </el-main>
-    <el-footer hiight="100%">Footer</el-footer>
+    <el-footer height="100%">
+      <my-footer/>
+    </el-footer>
   </el-container>
 </template>
 
 <script>
 import MyHeader from '@/components/public/header/index.vue'
+import MyFooter from '@/components/public/footer/index.vue'
+
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 }
 </script>
