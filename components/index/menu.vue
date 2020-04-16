db0b85c0672de5524b3a800eaf11faf264ab4e8d
xjw <857986029@qq.com>
2020-04-16 16:04:18 +0800

    首页菜单2

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -2,22 +2,38 @@
   <div class="m-menu">
     <dl
       class="nav"
+      @mouseleave="mouseleave"
       >
       <dt>全部分类</dt>
       <dd
         v-for="(item,idx) in menu"
         :key="idx"
+        @mouseenter="enter"
         >
         <i :class="item.type"/>{{ item.name }}<span class="arrow"/>
       </dd>
     </dl>
+    <div
+      v-if="kind"
+      class="detail"
+      @mouseenter="sover"
+      @mouseleave="sout">
+      <template v-for="(item,idx) in curdetail.child">
+        
+        <h4 :key="idx">{{ item.title }}</h4>
+        <span
+          v-for="v in item.child"
+          :key="v">{{ v }}</span>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {
+    return {   
+      kind: '',
       menu:[{
         type:'food',
         name:'美食',
@@ -41,6 +57,28 @@ export default {
         }]
       }]
     
+    }
+  },
+  computed: {
+    curdetail () {
+      return this.menu.filter(item => item.type === this.kind)[0]
+    }
+  },
+  methods: {
+    mouseleave () {
+      let self = this
+      self._timer = setTimeout(() => {
+        self.kind = ''
+      }, 150)
+    },
+    enter (e) {
+      this.kind = e.target.querySelector('i').className
+    },
+    sover (){
+      clearTimeout(this._timer)
+    },
+    sout (){
+      this.kind = ''
     }
   }
 }
