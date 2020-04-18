897aee8bfd4c6b41a5761f26cb207c623d34b948
xjw <857986029@qq.com>
2020-04-18 15:30:31 +0800

    城市定位接口ok

---
+++ server/interface/geo.js
@@ -0,0 +1,23 @@
+import Router from 'koa-router'
+import axios from './utils/axios'
+
+const router = new Router({
+  prefix: '/geo'
+})
+
+router.get('/getPosition', async ctx => {
+  let {status, data: {province, city}} = await axios.get('http://cp-tools.cn/geo/getPosition')
+  if (status === 200) {
+    ctx.body = {
+      province,
+      city
+    }
+  } else {
+    ctx.body = {
+      province: '',
+      city: ''
+    }
+  }
+})
+
+export default router
