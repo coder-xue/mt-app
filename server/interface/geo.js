75ebcfb1c066445260e7e32b9331dc1aa51e07dc
xjw <857986029@qq.com>
2020-04-18 21:26:45 +0800

    首页菜单nuxt渲染ok

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import axios from './utils/axios'
+import Menu from '../dbs/models/menu'
 
 const router = new Router({
   prefix: '/geo'
@@ -16,6 +17,23 @@ router.get('/getPosition', async ctx => {
     ctx.body = {
       province: '',
       city: ''
+    }
+  }
+})
+
+router.get('/menu', async ctx => {
+  // const result = await Menu.findOne()
+  // ctx.body = {
+  //   menu: result.menu
+  // }
+  let {status, data: {menu}} = await axios.get('http://cp-tools.cn/geo/menu')
+  if (status === 200) {
+    ctx.body = {
+      menu
+    }
+  } else {
+    ctx.body = {
+      menu: []
     }
   }
 })
--- store/index.js
+++ store/index.js
@@ -1,18 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import geo from './modules/geo'
+import home from './modules/home'
 
 Vue.use(Vuex)
 
 const store = () => new Vuex.Store({
   modules: {
-    geo
-  },
+    geo,
+    home
+  }, 
   actions: {
     async nuxtServerInit ({commit}, {req, app}) {
       console.log('刷新会再次执行')
+      // 获取城市定位
       const {status, data: {province, city}} = await app.$axios.get('/geo/getPosition')
       commit('geo/setPosition', status === 200 ? {province, city} : {province: '', city: ''})
+
+      // 获取菜单menu
+      const {status: status2, data: {menu}} = await app.$axios.get('/geo/menu')
+      commit('home/setMenu', status2 === 200 ? menu : [])
     }
   }
 })
